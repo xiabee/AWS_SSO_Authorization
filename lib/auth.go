13eb376a90e24eec7cadb83c077b1fc11d36d212
xiabee <xiabee@foxmail.com>
2023-06-26 15:08:12 +0800

    update

---
+++ lib/auth.go
@@ -0,0 +1,28 @@
+package lib
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssoadmin"
+	"fmt"
+)
+
+func Auth(sess *session.Session){
+	svc := ssoadmin.New(sess)
+
+	params := &ssoadmin.CreateAccountAssignmentInput{
+		InstanceArn:      aws.String("arn:aws:sso:::instance/ssoins-7758e707bb6ea352"),
+		TargetId:         aws.String("316218510314"),
+		TargetType:       aws.String("AWS_ACCOUNT"),
+		PermissionSetArn: aws.String("arn:aws:sso:::permissionSet/ssoins-7758e707bb6ea352/ps-cc8887ee3dd4c3d0"), // Dbaas-ViewOnly-Role
+		PrincipalType:    aws.String("USER"),
+		PrincipalId:      aws.String("95670a3c83-4c2e0132-6440-4040-8dd2-aa4d994ca926"), // yunjie.xiao@pingcap.com
+	}
+
+	_, err := svc.CreateAccountAssignment(params)
+	if err != nil {
+		fmt.Println("Failed:", err)
+		return
+	}
+
+	fmt.Println("Success!")
+}
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+import (
+	"lib"
+	//"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	//"github.com/aws/aws-sdk-go/service/sso"
+	//"github.com/aws/aws-sdk-go/service/identitystore"
+	//"github.com/aws/aws-sdk-go/service/ssoadmin"
+	//"fmt"
+)
+func main() {
+
+	sess, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	if err != nil {
+		panic(err)
+	}
+	lib.Auth(sess)
+
+	// svc := identitystore.New(sess, &aws.Config{
+	// 	Region:                       aws.String("ap-northeast-1"),
+	// 	Endpoint:                     aws.String("https://identitystore.ap-northeast-1.amazonaws.com"),
+	// 	CredentialsChainVerboseErrors: aws.Bool(true),
+	// })
+
+	// params := &identitystore.ListUsersInput{
+	// 	IdentityStoreId: aws.String("d-95670a3c83"),
+	// 	MaxResults: aws.Int64(100), // 设置每次返回的最大结果数
+	// }
+
+	// for {
+	// 	resp, err := svc.ListUsers(params)
+	// 	if err != nil {
+	// 		fmt.Println("Error2:", err)
+	// 		return
+	// 	}
+
+	// 	// 处理 resp.Users 中的用户结果
+	// 	for _, user := range resp.Users {
+	// 		fmt.Println(*user.UserName, *user.UserId)
+	// 	}
+	// 	// 检查是否还有更多结果
+	// 	if resp.NextToken != nil {
+	// 		params.NextToken = resp.NextToken // 设置下一页请求的令牌
+	// 	} else {
+	// 		break // 已获取所有结果，退出循环
+	// 	}
+	// }
+
+	// fmt.Println("All users retrieved!")
+}
